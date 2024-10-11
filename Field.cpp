a97e98e9e865f59855462938b7d89c11a4283b39
Polgraphik <gavrilithapavel4334@gmail.com>
2024-10-11 11:17:52 +0300

    fix

--- Field.cpp
+++ Field.cpp
@@ -1,5 +1,5 @@
 #include "Field.h"
 
-field_t::field_t(int width = 20, int height = 20) {}
-int field_t::get_width() const {}
-int field_t::get_height() const {}
+field_t::field_t(int width, int height) {}
+int field_t::get_width() const {return _width;}
+int field_t::get_height() const {return _height;}
